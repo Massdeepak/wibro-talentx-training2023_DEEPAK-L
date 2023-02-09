e06d625f24978109a09cdbe2b34bb7b7711a4f3f
Deepak L <90788256+Massdeepak@users.noreply.github.com>
2023-02-09 19:02:45 +0530

    Create digitSum odd-sum of odd digits in N.java

---
+++ Day-4/digitSum odd-sum of odd digits in N.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode
+{
+public int OddDigitsSum(int inpu1){
+int sum=0;
+while(input1!=0)
+{
+int n=input1%10;
+if(n%2!=0)
+sum+=n;
+input1/=10; 
+}
+return sum;
+}
